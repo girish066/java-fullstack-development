8fef45246555509ad103f3808327487cc5fae8f7
girish <girish@gmail.com>
2022-03-16 14:17:12 +0530

    insert into data

---
+++ phase4/frontend/first.js
@@ -0,0 +1,5 @@
+var a = 12;
+var b = 22;
+var fname = "alex";
+console.log(a + b);
+console.log(fname);
---
+++ phase4/frontend/first.ts
@@ -0,0 +1,8 @@
+let a:number =12;
+
+let b:number =22;
+
+let fname:string ="alex";
+
+console.log(a+b);
+console.log(fname);
